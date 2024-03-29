8a64aaeb9af9f4e4c2341618e9a39cd09443e45b
IYStoychev21 <IYStoychev21@codingburgas.bg>
2024-03-29 22:45:38 +0200

    Init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './routes/Home'
+
+function App() {
+    const BrowserRouter = createBrowserRouter([
+    { path: '/', element: <Home /> },
+  ])
+  
+  return (
+    <RouterProvider router={BrowserRouter} />
+  )
+}
+
+export default App
---
+++ frontend/src/routes/Home.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+    return (
+        <>
+            <div>
+                <Button>Hello World</Button>
+            </div>
+        </>
+    )
+}
