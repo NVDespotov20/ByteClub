fece476756558a67496438cb5d4408e40d3fb5c9
Alexander Manov <AEManov20@codingburgas.bg>
2024-03-30 01:33:28 +0200

    Add User model

--- backend/IdeaInvestigator.Data/Models/User.cs
+++ backend/IdeaInvestigator.Data/Models/User.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 
-namespace IdeaInvestigator.Data.Models
+namespace IdeaInvestigator.Data.Models;
+
+public class User : IdentityUser<Guid>
 {
-    internal class User
-    {
-    }
+    public override Guid Id { get; set; } = Guid.NewGuid();
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
 }
